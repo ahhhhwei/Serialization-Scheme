014d0de427306e3a16c3dee92a058b5669b54b88
ahhhhwei <202103151422@zjut.edu.cn>
2025-01-12 13:37:51 +0800

    commit

--- 1.Protobuf/code/Contacts2/Contact2.1/read.cc
+++ 1.Protobuf/code/Contacts2/Contact2.1/read.cc
@@ -3,16 +3,16 @@
 #include "contacts.pb.h"
 using namespace std;
 using namespace contacts;
+
 /**
- *
- 打印联系⼈列表
+ * 打印联系人列表。
  */
 void PrintfContacts(const Contacts &contacts)
 {
     for (int i = 0; i < contacts.contacts_size(); ++i)
     {
         const PeopleInfo &people = contacts.contacts(i);
-        cout << "-----------联系⼈" << i + 1 << "------------" << endl;
+        cout << "-----------联系人" << i + 1 << "------------" << endl;
         cout << "姓名：" << people.name() << endl;
         cout << "年龄：" << people.age() << endl;
         int j = 1;
@@ -23,26 +23,35 @@ void PrintfContacts(const Contacts &contacts)
         }
     }
 }
+
 int main(int argc, char *argv[])
 {
+    // GOOGLE_PROTOBUF_VERIFY_VERSION 宏用于验证是否意外链接到了与编译的头文件不兼容的库版本。如果检测到版本不匹配，程序将终止。注意，每个.pb.cc 文件在启动时都会自动调用此宏。在使用 C++ Protocol Buffer库之前执行此宏是一个很好的做法，但不是绝对必要的。
+
     GOOGLE_PROTOBUF_VERIFY_VERSION;
     if (argc != 2)
     {
-        cerr << "Usage:  " << argv[0] << "CONTACTS_FILE" << endl;
+        cerr << "用法:  " << argv[0] << " CONTACTS_FILE" << endl;
         return -1;
     }
-    // 以⼆进制⽅式读取contacts
+
+    // 以二进制方式读取联系人信息
     Contacts contacts;
     fstream input(argv[1], ios::in | ios::binary);
     if (!contacts.ParseFromIstream(&input))
     {
-        cerr << "Failed to parse contacts." << endl;
+        cerr << "解析联系人信息失败。" << endl;
         input.close();
         return -1;
     }
-    // 打印contacts
+
+    // 打印联系人信息
     PrintfContacts(contacts);
+
     input.close();
+
+    // 在程序结束时调用 ShutdownProtobufLibrary()，以释放Protocol Buffer库分配的所有全局对象。对于大多数程序来说这是不必要的，因为进程无论如何都要退出，并且操作系统会负责回收所有内存。但是，如果你使用了内存泄漏检查工具，或者编写的是可以由单个进程多次加载和卸载的库，那么你可能希望强制Protocol Buffers清理所有内容。
+
     google::protobuf::ShutdownProtobufLibrary();
     return 0;
 }
--- 1.Protobuf/code/Contacts2/Contact2.1/write.cc
+++ 1.Protobuf/code/Contacts2/Contact2.1/write.cc
@@ -5,24 +5,23 @@ using namespace std;
 using namespace contacts;
 
 /**
-  *
-  新增联系⼈
-  */
+ * 新增联系人信息。
+ */
 void AddPeopleInfo(PeopleInfo *people_info_ptr)
 {
-    cout << "------------新增联系⼈-------------" << endl;
-    cout << "请输⼊联系⼈姓名: ";
+    cout << "------------新增联系人-------------" << endl;
+    cout << "请输入联系人姓名: ";
     string name;
     getline(cin, name);
     people_info_ptr->set_name(name);
-    cout << "请输⼊联系⼈年龄:";
+    cout << "请输入联系人年龄:";
     int age;
     cin >> age;
     people_info_ptr->set_age(age);
     cin.ignore(256, '\n');
     for (int i = 1;; i++)
     {
-        cout << "请输⼊联系⼈电话" << i << "(只输⼊回⻋完成电话新增) : ";
+        cout << "请输入联系人电话" << i << "(仅输入回车完成电话添加) : ";
         string number;
         getline(cin, number);
         if (number.empty())
@@ -44,52 +43,52 @@ void AddPeopleInfo(PeopleInfo *people_info_ptr)
             phone->set_type(PeopleInfo_Phone_PhoneType::PeopleInfo_Phone_PhoneType_TEL);
             break;
         default:
-            cout << "⾮法选择，使用默认值！" << endl;
-                break;
+            cout << "非法选择，使用默认值！" << endl;
+            break;
         }
     }
-    cout << "----------添加联系⼈成功-----------" << endl;
+    cout << "----------添加联系人成功-----------" << endl;
 }
 
 int main(int argc, char *argv[])
 {
-    // GOOGLE_PROTOBUF_VERIFY_VERSION 宏:验证没有意外链接到与编译的头⽂件不兼容的库版本。如果检测到版本不匹配，程序将中⽌。注意，每个.pb.cc ⽂件在启动时都会⾃动调⽤此宏。在使⽤ C++ Protocol Buffer库之前执⾏此宏是⼀种很好的做法，但不是绝对必要的。
+    // GOOGLE_PROTOBUF_VERIFY_VERSION 宏用于验证是否意外链接到了与编译的头文件不兼容的库版本。如果检测到版本不匹配，程序将终止。注意，每个.pb.cc 文件在启动时都会自动调用此宏。在使用 C++ Protocol Buffer库之前执行此宏是一个很好的做法，但不是绝对必要的。
 
     GOOGLE_PROTOBUF_VERIFY_VERSION;
     if (argc != 2)
     {
-        cerr << "Usage:  " << argv[0] << " CONTACTS_FILE" << endl;
+        cerr << "用法:  " << argv[0] << " CONTACTS_FILE" << endl;
         return -1;
     }
     Contacts contacts;
-    // 先读取已存在的contacts
+    // 尝试读取已存在的联系人信息
     fstream input(argv[1], ios::in | ios::binary);
     if (!input)
     {
-        cout << argv[1] << ": File was not found.  Creating a new file." << endl;
+        cout << argv[1] << ": 文件未找到。创建一个新文件。" << endl;
     }
     else if (!contacts.ParseFromIstream(&input))
     {
-        cerr << "Failed to parse contacts." << endl;
+        cerr << "解析联系人信息失败。" << endl;
         input.close();
         return -1;
     }
-    // 新增⼀个联系⼈
+    // 添加一个新的联系人
 
     AddPeopleInfo(contacts.add_contacts());
-    // 向磁盘⽂件写⼊新的 contacts
+    // 将更新后的联系人信息写入磁盘文件
     fstream output(argv[1], ios::out | ios::trunc | ios::binary);
     if (!contacts.SerializeToOstream(&output))
     {
-        cerr << "Failed to write contacts." << endl;
+        cerr << "写入联系人信息失败。" << endl;
         input.close();
         output.close();
         return -1;
     }
     input.close();
     output.close();
-    // 在程序结束时调⽤ ShutdownProtobufLibrary()，为了删除Protocol Buffer库分配的所有全局对象。对于⼤多数程序来说这是不必要的，因为该过程⽆论如何都要退出，并且操作系统将负责回收其所有内存。但是，如果你使⽤了内存泄漏检查程序，该程序需要释放每个最后对象，或者你正在编写可以由单个进程多次加载和卸载的库，那么你可能希望强制使⽤Protocol Buffers来清理所有内容。
 
+    // 在程序结束时调用 ShutdownProtobufLibrary()，以释放Protocol Buffer库分配的所有全局对象。对于大多数程序来说这是不必要的，因为进程无论如何都要退出，并且操作系统会负责回收所有内存。但是，如果你使用了内存泄漏检查工具，或者编写的是可以由单个进程多次加载和卸载的库，那么你可能希望强制Protocol Buffers清理所有内容。
     google::protobuf::ShutdownProtobufLibrary();
     return 0;
 }
