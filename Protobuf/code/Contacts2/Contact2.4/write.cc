f2fd71dbb28358528578a85d26a57f0acb7345e7
ahhhhwei <202103151422@zjut.edu.cn>
2025-01-20 00:23:33 -0800

    通讯录2.4

---
+++ Protobuf/code/Contacts2/Contact2.4/write.cc
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <fstream>
+#include "contacts.pb.h"
+using namespace std;
+using namespace contacts;
+
+/**
+ * 新增联系人信息。
+ */
+void AddPeopleInfo(PeopleInfo *people_info_ptr)
+{
+    cout << "------------新增联系人-------------" << endl;
+    cout << "请输入联系人姓名: ";
+    string name;
+    getline(cin, name);
+    people_info_ptr->set_name(name);
+    cout << "请输入联系人年龄:";
+    int age;
+    cin >> age;
+    people_info_ptr->set_age(age);
+    cin.ignore(256, '\n');
+    for (int i = 1;; i++)
+    {
+        cout << "请输入联系人电话" << i << "(仅输入回车完成电话添加) : ";
+        string number;
+        getline(cin, number);
+        if (number.empty())
+        {
+            break;
+        }
+        PeopleInfo_Phone *phone = people_info_ptr->add_phone();
+        phone->set_number(number);
+        cout << "选择此电话类型（1、移动电话    2、固定电话）：";
+        int type;
+        cin >> type;
+        cin.ignore(256, '\n');
+        switch (type)
+        {
+        case 1:
+            phone->set_type(PeopleInfo_Phone_PhoneType::PeopleInfo_Phone_PhoneType_MP);
+            break;
+        case 2:
+            phone->set_type(PeopleInfo_Phone_PhoneType::PeopleInfo_Phone_PhoneType_TEL);
+            break;
+        default:
+            cout << "非法选择，使用默认值！" << endl;
+            break;
+        }
+    }
+
+    Address address;
+    cout << "请输入联系人家庭地址: ";
+    string home_address;
+    getline(cin, home_address);
+    address.set_home_address(home_address);
+    cout << "请输入联系人单位地址: ";
+    string unit_address;
+    getline(cin, unit_address);
+    address.set_unit_address(unit_address);
+    google::protobuf::Any *data = people_info_ptr->mutable_data();
+    data->PackFrom(address);
+
+    cout << "请选择要添加的其他联系方式（1.qq   1.微信）：";
+    int other_contact;
+    cin >> other_contact;
+    cin.ignore(256, '\n');
+    if (1 == other_contact)
+    {
+        cout << "请输入联系人qq号：";
+        string qq;
+        getline(cin, qq);
+        people_info_ptr->set_qq(qq);
+    }
+    else if (2 == other_contact)
+    {
+        cout << "请输入联系人微信号：";
+        string wechat;
+        getline(cin, wechat);
+        people_info_ptr->set_wechat(wechat);
+    }
+    else
+    {
+        cout << "选择有误，设置其他联系方式失败！" << endl;
+    }
+
+    for (int i = 1;; i++)
+    {
+        cout << "请输入备注" << "标题（只输入回车完成备注新增）：";
+        string remark_key;
+        getline(cin, remark_key);
+        if (remark_key.empty())
+        {
+            break;
+        }
+
+        cout << "请输入备注" << i << "内容：" << endl;
+        string remark_value;
+        getline(cin, remark_value);
+        people_info_ptr->mutable_remark()->insert({remark_key, remark_value});
+    }
+
+    cout << "----------添加联系人成功-----------" << endl;
+}
+
+int main(int argc, char *argv[])
+{
+    // GOOGLE_PROTOBUF_VERIFY_VERSION 宏用于验证是否意外链接到了与编译的头文件不兼容的库版本。如果检测到版本不匹配，程序将终止。注意，每个.pb.cc 文件在启动时都会自动调用此宏。在使用 C++ Protocol Buffer库之前执行此宏是一个很好的做法，但不是绝对必要的。
+
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    if (argc != 2)
+    {
+        cerr << "用法:  " << argv[0] << " CONTACTS_FILE" << endl;
+        return -1;
+    }
+    Contacts contacts;
+    // 尝试读取已存在的联系人信息
+    fstream input(argv[1], ios::in | ios::binary);
+    if (!input)
+    {
+        cout << argv[1] << ": 文件未找到。创建一个新文件。" << endl;
+    }
+    else if (!contacts.ParseFromIstream(&input))
+    {
+        cerr << "解析联系人信息失败。" << endl;
+        input.close();
+        return -1;
+    }
+    // 添加一个新的联系人
+
+    AddPeopleInfo(contacts.add_contacts());
+    // 将更新后的联系人信息写入磁盘文件
+    fstream output(argv[1], ios::out | ios::trunc | ios::binary);
+    if (!contacts.SerializeToOstream(&output))
+    {
+        cerr << "写入联系人信息失败。" << endl;
+        input.close();
+        output.close();
+        return -1;
+    }
+    input.close();
+    output.close();
+
+    // 在程序结束时调用 ShutdownProtobufLibrary()，以释放Protocol Buffer库分配的所有全局对象。对于大多数程序来说这是不必要的，因为进程无论如何都要退出，并且操作系统会负责回收所有内存。但是，如果你使用了内存泄漏检查工具，或者编写的是可以由单个进程多次加载和卸载的库，那么你可能希望强制Protocol Buffers清理所有内容。
+    google::protobuf::ShutdownProtobufLibrary();
+    return 0;
+}
